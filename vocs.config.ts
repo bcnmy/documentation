b924c2046d70f16fcbc0c8980d9100edb0026168
Joe Pegler <joepegler123@gmail.com>
2024-07-12 13:32:18 +0100

    chore: getting started

---
+++ vocs.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vocs'
+
+export default defineConfig({
+  description: 'Build reliable apps & libraries with lightweight, composable, and type-safe modules that interface with smart accounts',
+  title: 'Docs',
+  rootDir: './docs',
+  sidebar: [
+    {
+      text: 'Getting Started',
+      link: '/getting-started',
+    },
+  ],
+})
